d106fa6a65c5ea6b420566645d926426bcad35d7
Doan Minh Duy <duydoan812@gmail.com>
2025-03-23 00:29:03 +0700

    Update Commit

--- Using Funtion To Find Max In Array_VS/Program.cs
+++ Using Funtion To Find Max In Array_VS/Program.cs
@@ -22,18 +22,23 @@
         {
             Console.WriteLine("Enter your list (Each number has space between them): ");
             Console.WriteLine("Press 0 to exit: ");
-            while (true)
+            
+            while (true) //Tao vong lap sao cho user chi ra duoc ket qua khi nhap gia tri dung
             {
                 {
+                    //Truong hop user nhan 0 de thoat chuong trinh
                     string input = Console.ReadLine();
                     if (int.TryParse(input, out int exitvalue) && exitvalue == 0 )
                     {
                         Environment.Exit(exitvalue);
                         break;
                     }
+                    //Truong hop user nhap day ky tu
                     else
                     {
+                        //Kiem tra xem day ki tu co dung la chi co dau cach va so nguyen khong
                         bool IsInt = input.Replace(" ", "").All(char.IsDigit);
+                        //Neu dung chi co dau cach va so nguyen => Chay phuong thuc tinh toan
                         if (IsInt)
                         {
                             int[] numbers = input.Split(' ')
